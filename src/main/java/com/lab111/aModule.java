087794392b04eb33a56bfc689a6358141e1e7d6e
Andy Theodorko Ed <andriy.fedorko01@gmail.com>
2021-09-08 22:45:56 +0300

    add empty a*.java class to test deleting of files

---
+++ src/main/java/com/lab111/aModule.java
@@ -0,0 +1,27 @@
+package com.lab111;
+
+/**
+ * Template first class.
+ * @author Alex
+ *
+ */
+public final class aModule {
+
+  /**
+   * Constructor.
+   *
+   */
+  private aModule() {
+    super();
+  }
+  
+  /**
+   * Invokes at application startup.
+   * @param args Parameters from command line
+   */
+  public static void main(final String[] args) {
+    System.out.println("This is template example for main class");
+
+  }
+
+}

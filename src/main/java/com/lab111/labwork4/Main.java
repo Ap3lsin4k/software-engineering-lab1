09562baa088cc44ae24bcaaefb56bff1f0d4294f
Andy Theodorko <andy@pop-os.localdomain>
2021-10-30 17:11:39 +0300

    delete everything from previous practice

--- src/main/java/com/lab111/labwork4/Main.java
+++ src/main/java/com/lab111/labwork4/Main.java
@@ -3,7 +3,7 @@ package com.lab111.labwork4;
 /**
  * Class to draw primitix
  * @author Andy
- * @version 4.06
+ * @version 5.01
  */
 // Java program to draw a line in Applet
 
@@ -14,22 +14,12 @@ public class Main {
      * @param a
      */
     public static void main(String[] a) {
-        RectangleComposite big = new RectangleComposite(new PointFromCenter(0, 0), 100, 100);
-        RectangleComposite small = new RectangleComposite(new PointFromCenter(50, 120), 50, 50);
-
-        clientCode(new RectangleComposite[]{big, small});
-
-        LinePrimitive line = new LinePrimitive();
-        line.drawFromTopLeft(new PointFromTopLeft(0, 0), new PointFromTopLeft(20, 40));
+        System.out.println("SE lab");
     }
 
     /**
      * client Code only works with Target interface. Client draw can draw from center. It doesn't know about LinePrimitive.
-     * @param shapes
      */
-    public static void clientCode(Target[] shapes) {
-        for (Target drawable : shapes) {
-            drawable.drawFromCenter();
-        }
+    public static void clientCode() {
     }
 }
--- src/main/java/com/lab111/labwork4/Target.java
+++
@@ -1,8 +0,0 @@
-package com.lab111.labwork4;
-
-/**
- * interface declares method for drawing shapes (e.g. rectangle). All clients are bind to this interface.
- */
-public interface Target {
-    void drawFromCenter();
-}

508ac47eccd3e2dd7289c489ba3e40d2f9e2f2d4
Andy Theodorko <andy@pop-os.localdomain>
2021-11-11 21:58:01 +0200

    generate javadoc and png

---
+++ Javadoc 6/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.lab111.labwork6","l":"ButtonLeaf"},{"p":"com.lab111.labwork6","l":"HandlerComponent"},{"p":"com.lab111.labwork6","l":"ImageLeaf"},{"p":"com.lab111.labwork6","l":"Main"},{"p":"com.lab111.labwork6","l":"PanelComposite"}];updateSearchResults();
